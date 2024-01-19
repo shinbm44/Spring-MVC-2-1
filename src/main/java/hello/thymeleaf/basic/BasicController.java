b4d2f18bbfb8fc45d945c56e1633731d505e261b
hyunjin_byun <shinbm219@naver.com>
2024-01-19 16:32:04 +0900

    [섹션1]타임리프:블록단위 설정

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -127,6 +127,13 @@ public class BasicController {
         return "basic/comments";
     }
 
+    @GetMapping("/block")
+    public String block(Model model) {
+        addUser(model);
+        return "basic/block";
+    }
+
+
     @Data
     static class User {
         private String username;
