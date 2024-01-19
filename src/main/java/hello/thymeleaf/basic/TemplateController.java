566d98f7a4a0cda67158fb816e8567b85c9a697b
hyunjin_byun <shinbm219@naver.com>
2024-01-19 17:30:44 +0900

    [섹션1]타임리프:레이아웃1(ìê³공통부분재활용)

--- src/main/java/hello/thymeleaf/basic/TemplateController.java
+++ src/main/java/hello/thymeleaf/basic/TemplateController.java
@@ -14,4 +14,9 @@ public class TemplateController {
 
         return "template/fragment/fragmentMain";
     }
+
+    @GetMapping("/layout")
+    public String layout() {
+        return "/template/layout/layoutMain";
+    }
 }
