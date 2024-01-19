44641862042afa7e82eba774b805c2ed2f7fdada
hyunjin_byun <shinbm219@naver.com>
2024-01-19 16:27:10 +0900

    [섹션1]타임리프:주석

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -121,6 +121,12 @@ public class BasicController {
         model.addAttribute("users", list);
     }
 
+    @GetMapping("/comments")
+    public String comments(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/comments";
+    }
+
     @Data
     static class User {
         private String username;
